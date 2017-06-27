f7257fc500de167be3559f39294276509853064e
Scott O'Hara <scott@oharagroup.net>
2017-06-28 09:50:25 +1000

    Fixed header and favicon

--- src/addons/salesamount.js
+++ src/addons/salesamount.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const AddOnSalesAmount = ({amount}) => (<div className="addon-sales-amount">{+amount === 0 || isNaN(+amount) ? "-" : amount.toLocaleString("en-au", {style: "currency", currency: "AUD"})}</div>);
+
+AddOnSalesAmount.propTypes = {
+	amount: PropTypes.number
+}
 
 export default AddOnSalesAmount;
